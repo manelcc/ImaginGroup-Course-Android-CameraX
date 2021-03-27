488c30e26c9ac774dd0529259ebd59795d339943
manuel cabezas caldero <manelcc72@gmail.com>
2021-03-27 13:03:37 +0100

    GRADLE: add activity and fragment

--- app/src/main/java/es/imagingroup/exampleviewmodelhiltdb/presentation/view/fragment/LoginFragment.kt
+++ app/src/main/java/es/imagingroup/exampleviewmodelhiltdb/presentation/view/fragment/LoginFragment.kt
@@ -22,8 +22,8 @@ class LoginFragment:Fragment() {
         return binding.root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
     }
 }
